060d50c0517854a79b2249544ac612cb2f4e07b5
manbz <manon.bezard@ensc.fr>
2015-03-12 18:37:14 +0100

    manon 12/03

---
+++ PageMembre.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+include 'Header.php';
+?>
+
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="Style-general.css" rel="stylesheet">
+    <link href="css/dashboard.css" rel="stylesheet">
+  </head>
+    
+  <body>
+
+      <div  class="container-fluid">
+      <div  class="row">
+        <div id="menu" class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Mes évènements <span class="sr-only">(current)</span></a></li>
+            <li><a href="CreerGroupe.php" role="button">Créer un groupe</a></li>
+            <li><a href="CreerEvent.php" role="button">Créer un évènement</a></li>
+            <li><a href="modifierMonProfil.php" role="button">Modifier mon profil</a></li>
+            <li><a href="modifierMotDePasse.php" role="button">Modifier mon mot de passe</a></li>
+          </ul>
+        </div>
+      </div>
+        </div>
+
+      <h2 id="lignesTitre" class="sub-header">Je participe aux évènements</h2>
+      <div id="lignes" class="table-responsive">
+          <table id="largeurLigne" class="table table-striped">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Nom</th>
+                  <th>Lieu</th>
+                  <th>Prix</th>
+               </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>12/02/2016</td>
+                  <td>Casse Noisette</td>
+                  <td>Grand opéra</td>
+                  <td>35€</td>
+                </tr>
+                <tr>
+                  <td>1,002</td>
+                  <td>amet</td>
+                  <td>consectetur</td>
+                  <td>adipiscing</td>
+
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>Integer</td>
+                  <td>nec</td>
+                  <td>odio</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>libero</td>
+                  <td>Sed</td>
+                  <td>cursus</td>
+
+                </tr>
+                <tr>
+                  <td>1,004</td>
+                  <td>dapibus</td>
+                  <td>diam</td>
+                  <td>Sed</td>
+
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      
+      
+  </body>
+
+
+</html>
+
